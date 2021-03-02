c72cec9e9bf4261f43e73aec3bfef40047e6d409
CarrascoII <tomasbccarrasco@gmail.com>
2021-03-02 15:32:56 +0000

    Updated footprint checker. #32

--- l-tls/check_footprint.sh
+++ l-tls/check_footprint.sh
@@ -1,29 +1,40 @@
 #!/bin/sh
-EXTENTION=../mbedtls/library/
+
+TLS_DIR=../mbedtls/library
 CRYPTO=libmbedcrypto.a
 TLS=libmbedtls.a
 X509=libmbedx509.a
 
-MEASUREMENT=measurement/library/libmeasurement.a
-SERVER=tls_psk/server.out
-CLIENT=tls_psk/client.out
+MSR_DIR=measurement/library
+MEASUREMENT=libmeasurement.a
 
-CRYPTO_SIZE=$(stat -c%s "${EXTENTION}/${CRYPTO}")
-TLS_SIZE=$(stat -c%s "${EXTENTION}/${TLS}")
-X509_SIZE=$(stat -c%s "${EXTENTION}/${X509}")
+CORE_DIR=mbedcore/library
+CORE=libmbedcore.a
 
+# SERVER=tls_psk/server.out
+# CLIENT=tls_psk/client.out
 
-MEASUREMENT_SIZE=$(stat -c%s "${MEASUREMENT}")
-SERVER_SIZE=$(stat -c%s "${SERVER}")
-CLIENT_SIZE=$(stat -c%s "${CLIENT}")
+CRYPTO_SIZE=$(stat -c%s "${TLS_DIR}/${CRYPTO}")
+TLS_SIZE=$(stat -c%s "${TLS_DIR}/${TLS}")
+X509_SIZE=$(stat -c%s "${TLS_DIR}/${X509}")
 
-echo "Size of ${CRYPTO} = $((${CRYPTO_SIZE}/1024)).$(((${CRYPTO_SIZE}%1024)*1000/1024)) KiB."
-echo "Size of ${TLS}    = $((${TLS_SIZE}/1024)).$(((${TLS_SIZE}%1024)*1000/1024)) KiB."
-echo "Size of ${X509}   = $((${X509_SIZE}/1024)).$(((${X509_SIZE}%1024)*1000/1024)) KiB."
+MEASUREMENT_SIZE=$(stat -c%s "${MSR_DIR}/${MEASUREMENT}")
 
+CORE_SIZE=$(stat -c%s "${CORE_DIR}/${CORE}")
 
-echo "Size of ${MEASUREMENT}  = $((${MEASUREMENT_SIZE}/1024)).$(((${MEASUREMENT_SIZE}%1024)*1000/1024)) KiB."
-echo "Size of ${SERVER}  = $((${SERVER_SIZE}/1024)).$(((${SERVER_SIZE}%1024)*1000/1024)) KiB."
-echo "Size of ${CLIENT}  = $((${CLIENT_SIZE}/1024)).$(((${CLIENT_SIZE}%1024)*1000/1024)) KiB."
+# SERVER_SIZE=$(stat -c%s "${SERVER}")
+# CLIENT_SIZE=$(stat -c%s "${CLIENT}")
 
-echo "Total = $(((${CRYPTO_SIZE} + ${TLS_SIZE} + ${X509_SIZE} + ${MEASUREMENT_SIZE} + ${SERVER_SIZE} + ${CLIENT_SIZE})/1024)).$((((${CRYPTO_SIZE} + ${TLS_SIZE} + ${X509_SIZE} + ${MEASUREMENT_SIZE} + ${SERVER_SIZE} + ${CLIENT_SIZE})%1024)*1000/1024)) KiB."
+echo "Size of ${CRYPTO}  = $((${CRYPTO_SIZE}/1024)).$(((${CRYPTO_SIZE}%1024)*1000/1024)) KiB."
+echo "Size of ${TLS}     = $((${TLS_SIZE}/1024)).$(((${TLS_SIZE}%1024)*1000/1024)) KiB."
+echo "Size of ${X509}    = $((${X509_SIZE}/1024)).$(((${X509_SIZE}%1024)*1000/1024)) KiB."
+
+echo "Size of ${MEASUREMENT} = $((${MEASUREMENT_SIZE}/1024)).$(((${MEASUREMENT_SIZE}%1024)*1000/1024)) KiB."
+
+echo "Size of ${CORE}    = $((${CORE_SIZE}/1024)).$(((${CORE_SIZE}%1024)*1000/1024)) KiB."
+
+# echo "Size of ${SERVER}  = $((${SERVER_SIZE}/1024)).$(((${SERVER_SIZE}%1024)*1000/1024)) KiB."
+# echo "Size of ${CLIENT}  = $((${CLIENT_SIZE}/1024)).$(((${CLIENT_SIZE}%1024)*1000/1024)) KiB."
+
+# echo "Total = $(((${CRYPTO_SIZE} + ${TLS_SIZE} + ${X509_SIZE} + ${MEASUREMENT_SIZE} + ${SERVER_SIZE} + ${CLIENT_SIZE})/1024)).$((((${CRYPTO_SIZE} + ${TLS_SIZE} + ${X509_SIZE} + ${MEASUREMENT_SIZE} + ${SERVER_SIZE} + ${CLIENT_SIZE})%1024)*1000/1024)) KiB."
+echo "Total                    = $(((${CRYPTO_SIZE} + ${TLS_SIZE} + ${X509_SIZE} + ${MEASUREMENT_SIZE} + ${CORE_SIZE})/1024)).$((((${CRYPTO_SIZE} + ${TLS_SIZE} + ${X509_SIZE} + ${MEASUREMENT_SIZE} + ${CORE_SIZE})%1024)*1000/1024)) KiB."
