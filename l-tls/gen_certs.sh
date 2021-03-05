3c5cb6dfbb69424a737b647dd880d5eea92fd451
CarrascoII <tomasbccarrasco@gmail.com>
2021-03-05 22:51:22 +0000

    Generated keys and certs for RSA with different key size. #29

---
+++ l-tls/gen_certs.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+GEN_KEY=../mbedtls/programs/pkey/gen_key
+WRITE_CERT=../mbedtls/programs/x509/cert_write
+OUTPUT_PATH=examples
+DEADLINE=20221231235959
+RSA_KEY_SIZES=(1024 2048 4096 8192)
+EC_CURVE_192=(192 secp192r1)
+EC_CURVE_224=(224 secp224r1)
+EC_CURVE_384=(384 secp384r1)
+EC_CURVE_521=(521 secp521r1)
+EC_CURVES=(EC_CURVE_192 EC_CURVE_224 EC_CURVE_384 EC_CURVE_521)
+ENDPOINTS=(srv cli)
+# TODO: verify size of the keys in each curve. EC generation uses option "ec_curve="
+# EC_KEY_SIZES=(160 224 256 512)
+# ECC_KEY_SIZES=(192 240 320 512)
+# KEY_SIZES=(RSA_KEY_SIZES ECC_KEY_SIZES)
+
+# declare -n sizes
+
+# for sizes in ${EC_CURVES[@]}
+# do
+#     for size in ${sizes[@]}
+#     do
+#         echo "Printing ${size}"
+#     done
+# done
+
+find examples/ -mindepth 0 -type f -name '*.*' -delete
+
+for size in ${RSA_KEY_SIZES[*]}
+do
+    ${GEN_KEY} type=rsa rsa_keysize=${size} filename=${OUTPUT_PATH}/ca_rsa_${size}.key
+    ${WRITE_CERT} selfsign=1 issuer_key=${OUTPUT_PATH}/ca_rsa_${size}.key issuer_name=CN=myca,O=myorganization,C=NL \
+                not_after=${DEADLINE} is_ca=1 max_pathlen=0 output_file=${OUTPUT_PATH}/ca_rsa_${size}.crt
+
+    for end in ${ENDPOINTS[*]}
+    do
+        ${GEN_KEY} type=rsa rsa_keysize=${size} filename=${OUTPUT_PATH}/${end}_rsa_${size}.key
+        ${WRITE_CERT} issuer_crt=${OUTPUT_PATH}/ca_rsa_${size}.crt issuer_key=${OUTPUT_PATH}/ca_rsa_${size}.key \
+                    subject_key=${OUTPUT_PATH}/${end}_rsa_${size}.key issuer_name=CN=my${end},O=myorganization,C=NL \
+                    not_after=${DEADLINE} output_file=${OUTPUT_PATH}/${end}_rsa_${size}.crt
+    done
+done
+
+# declare -n curve
+
+# for curve in ${EC_CURVES[@]}
+#     echo "${curve[0]}"
+#     ${GEN_KEY} type=ec ec_curve=${curve[1]} filename=${OUTPUT_PATH}/ca_ec_${curve[0]}.key
+#     ${WRITE_CERT} selfsign=1 issuer_key=${OUTPUT_PATH}/ca_ec_${curve[0]}.key issuer_name=CN=myca,O=myorganization,C=NL \
+#                 not_after=${DEADLINE} is_ca=1 max_pathlen=0 output_file=${OUTPUT_PATH}/ca_ec_${curve[0]}.crt
+
+#     for end in ${ENDPOINTS[*]}
+#     do
+#         ${GEN_KEY} type=ec ec_curve=${curve[1]} filename=${OUTPUT_PATH}/${end}_ec_${curve[0]}.key
+#         ${WRITE_CERT} issuer_crt=${OUTPUT_PATH}/ca_ec_${curve[0]}.crt issuer_key=${OUTPUT_PATH}/ca_ec_${curve[0]}.key \
+#                     subject_key=${OUTPUT_PATH}/${end}_ec_${curve[0]}.key issuer_name=CN=my${end},O=myorganization,C=NL \
+#                     not_after=${DEADLINE} output_file=${OUTPUT_PATH}/${end}_ec_${curve[0]}.crt
+#     done
+# done
